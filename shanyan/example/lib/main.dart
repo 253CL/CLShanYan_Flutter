bdb0a6f660940b08dd516460f14fa97ff3e47d3e
ydong <863001315@qq.com>
2023-12-19 13:59:05 +0800

    升级iOS SDK

--- shanyan/example/lib/main.dart
+++ shanyan/example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shanyan/shanYanResult.dart';
